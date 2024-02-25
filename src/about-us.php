05113f61a12c5648047a7c634552d79caa2afd20
iballi2017 <iballi201@gmail.com>
2024-02-25 15:03:54 +0100

    implement about-us and pricing pages

---
+++ src/about-us.php
@@ -0,0 +1,95 @@
+<?php include(__DIR__ . "./includes/header.php"); ?>
+
+<div class="bg-banner-1">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <ul class="breadcrumb py-2">
+            <li><a href="#">Home</a></li>
+            <li>About Us</li>
+        </ul>
+        <div class="py-16">
+            <h1 class="font-bold text-center text-4xl sm:text-5xl text-dark-200">About Us</h1>
+        </div>
+    </div>
+</div>
+
+
+
+<section class="bg-accent">
+    <div class="mx-auto max-w-7xl px-2 py-8 sm:pt-16 sm:px-6 lg:px-8">
+        <ul class="grid grid-cols-12 items-center gap-y-10 sm:gap-y-8">
+
+            <li class="col-span-12">
+                <div class="grid grid-cols-12 items-center gap-4 sm:gap-8">
+                    <div class="col-span-12 sm:col-span-6 fade-in" data-delay="200">
+                        <img src="./images/about-us-main-featured-image.png" alt="About-us featured image">
+                    </div>
+                    <div class="col-span-12 sm:col-span-6 fade-in" data-delay="400">
+                        <p class="text-white sm:text-lg mb-2">Vanlagos.com is a reliable vehicle hiring company that provides premium services to our customers at an affordable and competitive market price. In the areas of safety, timeliness, efficiency and excellent customer service, Vanlagos provides outstanding offers for you. We also have tools in place to provide support and assist you in a situation which is why you should choose us.</p>
+
+                        <p class="text-white sm:text-lg">Our rigorous vetting process has ensured that all our drivers are qualified and in compliance with all the regulations of applicable bodies.
+                            When you book a bus/van with us, your safety is assured as a result of the trust you have established with us to get you to your location.</p>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</section>
+
+
+<section>
+    <h2 class="sr-only">Our Core Values</h2>
+    <div class="mx-auto max-w-7xl px-2 py-8 mb-16 sm:pt-16 sm:px-6 lg:px-8">
+        <ul class="grid grid-cols-12 items-center gap-y-10 sm:gap-y-8">
+
+            <li class="col-span-12">
+                <div class="grid grid-cols-12 items-center gap-4 sm:gap-8">
+                    <div class="col-span-12 sm:col-span-6 fade-in" data-delay="200">
+                        <img src="./images/punctuality-featured-image-alt.png" alt="Punctuality featured image">
+                    </div>
+                    <div class="col-span-12 sm:col-span-6 fade-in" data-delay="400">
+                        <h4 class="ff-headline font-bold text-2xl sm:text-4xl text-primary mb-2">Punctuality</h4>
+                        <p class="text-dark sm:text-lg">At Vanlagos, we understand the value of time. That's why
+                            punctuality is ingrained in every
+                            aspect of our service. From the moment you make a booking to the final destination, we
+                            prioritize timeliness to ensure your journey is seamless and stress-free. </p>
+                    </div>
+                </div>
+            </li>
+
+            <li class="col-span-12">
+                <div class="grid grid-cols-12 items-center gap-4 sm:gap-8">
+                    <div class="col-span-12 sm:col-span-6 order-2 sm:order-1 fade-in" data-delay="350">
+                        <h4 class="ff-headline font-bold text-2xl sm:text-4xl text-primary mb-2">Air Conditioned Buses</h4>
+                        <p class="text-dark sm:text-lg">Embark on any journey, be it work commute, sightseeing, or group
+                            outings, and find our buses offering a cool, refreshing ambiance, even under scorching heat.
+                            Bid farewell to sweltering discomfort as you embrace a relaxing ride, where you can sit
+                            back, unwind, and enjoy the journey in absolute comfort.</p>
+                    </div>
+                    <div class="col-span-12 sm:col-span-6 order-1 sm:order-2 fade-in" data-delay="400">
+                        <img src="./images/air-conditioned-buses-featured-image-alt.png" alt="Air Conditioned Buses featured image">
+                    </div>
+                </div>
+            </li>
+
+            <li class="col-span-12">
+                <div class="grid grid-cols-12 items-center gap-4 sm:gap-8">
+                    <div class="col-span-12 sm:col-span-6 fade-in" data-delay="450">
+                        <img src="./images/comfortable-seats-and-leg-space-featured-image-alt.png" alt="Comfortable seats & leg space featured image">
+                    </div>
+                    <div class="col-span-12 sm:col-span-6 fade-in" data-delay="500">
+                        <h4 class="ff-headline font-bold text-2xl sm:text-4xl text-primary mb-2">Comfortable seats & leg space</h4>
+                        <p class="text-dark sm:text-lg">Ensuring comfort for all travelers, our buses feature spacious,
+                            cozy seats with ample legroom, catering to passengers of every size, whether embarking on a
+                            short commute or a long-distance journey, ensuring a comfortable ride with our plush seating
+                            arrangements.</p>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</section>
+
+
+<?php include(__DIR__ . "./includes/how-it-works.php"); ?>
+
+<?php include(__DIR__ . "./includes/footer.php"); ?>
--- src/includes/header.php
+++ src/includes/header.php
@@ -78,15 +78,15 @@
                         <div class="hidden sm:ml-6 sm:block">
                             <div class="flex space-x-4">
                                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                <a href="#"
+                                <a href="./"
                                     class="bg-gray-100 text-dark hover:text-gray-700 rounded-md px-3 py-2 text-sm font-medium"
                                     aria-current="page">Home</a>
-                                <a href="#"
+                                <a href="./about-us.php"
                                     class="text-dark hover:bg-gray-100 hover:text-gray-700 rounded-md px-3 py-2 text-sm font-medium">About
                                     Us</a>
-                                <a href="#"
+                                <a href="./pricing.php"
                                     class="text-dark hover:bg-gray-100 hover:text-gray-700 rounded-md px-3 py-2 text-sm font-medium">Pricing</a>
-                                <a href="#"
+                                <a href="./contact-us.php"
                                     class="text-dark hover:bg-gray-100 hover:text-gray-700 rounded-md px-3 py-2 text-sm font-medium">Contact
                                     Us</a>
                             </div>
@@ -154,12 +154,14 @@
             <div x-show="isOpen" class="sm:hidden absolute bg-gray-800 w-full z-10" id="mobile-menu">
                 <div class="space-y-1 px-2 pb-3 pt-2">
                     <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                    <a href="#" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
-                        aria-current="page">Dashboard</a>
-                    <a href="#"
-                        class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
-                    <a href="#"
-                        class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
+                    <a href="./" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
+                        aria-current="page">Home</a>
+                    <a href="./about-us.php"
+                        class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">About Us</a>
+                    <a href="./pricing.php"
+                        class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Pricing</a>
+                    <a href="./contact-us.php"
+                        class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Contact Us</a>
                     <a href="./login.php"
                         class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Sign
                         In / Sign Up</a>
