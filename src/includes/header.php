e276122d0667422af7ab1af5cc0fa13b8e4918da
iballidev <iballi.dev@gmail.com>
2024-02-29 18:14:57 +0100

    update

--- src/includes/header.php
+++ src/includes/header.php
@@ -136,7 +136,7 @@
                                     role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button"
                                     tabindex="-1">
                                     <!-- Active: "bg-gray-100", Not Active: "" -->
-                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem"
+                                    <a href="./user-profile.php" class="block px-4 py-2 text-sm text-gray-700" role="menuitem"
                                         tabindex="-1" id="user-menu-item-0">Your Profile</a>
                                     <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem"
                                         tabindex="-1" id="user-menu-item-1">Settings</a>
---
+++ src/user-profile.php
@@ -0,0 +1,25 @@
+<?php include(__DIR__ . "./includes/header.php"); ?>
+
+<div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+
+
+    <!-- user account navigation -->
+    <?php include(__DIR__ . "./includes/user-account-navigation.php")?>
+    <!--  -->
+    <div class="my-4"></div>
+
+    <div class="mdc-card">
+        <h1
+            class="ff-headline font-semibold capitalize text-xl bg-dark text-white py-2 px-4 rounded-t-md sm:rounded-t-lg">
+            Personal Information
+        </h1>
+        <div class="p-4">
+            hello
+        </div>
+    </div>
+
+    <div class="my-4"></div>
+
+</div>
+
+<?php include(__DIR__ . "./includes/footer.php"); ?>
