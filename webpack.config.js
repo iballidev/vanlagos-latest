4960003f0a0b06fd311c406b9516b9a3e0f763dd
iballi2017 <iballi201@gmail.com>
2024-03-02 22:40:14 +0100

    implement material design component locally

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,86 @@
-// module.exports = [{
-//     entry: './app.scss',
-//     output: {
-//         // This is necessary for webpack to compile
-//         // But we never use style-bundle.js
-//         filename: 'style-bundle.js',
-//     },
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.scss$/,
-//                 use: [
-//                     {
-//                         loader: 'file-loader',
-//                         options: {
-//                             name: 'bundle.css',
-//                         },
-//                     },
-//                     { loader: 'extract-loader' },
-//                     { loader: 'css-loader' },
-//                     {
-//                         loader: 'sass-loader',
-//                         options: {
-//                             // Prefer Dart Sass
-//                             implementation: require('sass'),
-
-//                             // See https://github.com/webpack-contrib/sass-loader/issues/804
-//                             webpackImporter: false,
-//                             sassOptions: {
-//                               includePaths: ['./node_modules']
-//                             },
-//                         },
-//                     },
-//                 ]
-//             }
-//         ]
-//     },
-// }];
-
+const autoprefixer = require('autoprefixer');
 
 const path = require('path');
 
-module.exports = {
-    entry: './src/javascripts/index.ts',
+function tryResolve_(url, sourceFilename) {
+    // Put require.resolve in a try/catch to avoid node-sass failing with cryptic libsass errors
+    // when the importer throws
+    try {
+        return require.resolve(url, { paths: [path.dirname(sourceFilename)] });
+    } catch (e) {
+        return '';
+    }
+}
+
+function tryResolveScss(url, sourceFilename) {
+    // Support omission of .scss and leading _
+    const normalizedUrl = url.endsWith('.scss') ? url : `${url}.scss`;
+    return tryResolve_(normalizedUrl, sourceFilename) ||
+        tryResolve_(path.join(path.dirname(normalizedUrl), `_${path.basename(normalizedUrl)}`),
+            sourceFilename);
+}
+
+function materialImporter(url, prev) {
+    if (url.startsWith('@material')) {
+        const resolved = tryResolveScss(url, prev);
+        return { file: resolved || url };
+    }
+    return { file: url };
+}
+
+module.exports = [{
+    entry: ['./src/stylesheets/sass/main.scss', './src/javascripts/mdc.js'],
     output: {
+        // This is necessary for webpack to compile
+        // But we never use style-bundle.js
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
     },
     module: {
         rules: [
             {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'bundle.css',
+                        },
+                    },
+                    { loader: 'extract-loader' },
+                    { loader: 'css-loader' },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    autoprefixer()
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            // Prefer Dart Sass
+                            implementation: require('sass'),
+
+                            // See https://github.com/webpack-contrib/sass-loader/issues/804
+                            webpackImporter: false,
+                            sassOptions: {
+                                importer: materialImporter,
+                                includePaths: ['./node_modules']
+                            },
+                        },
+                    },
+                ]
+            }, {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                },
+            }
+        ]
     },
-};
+    mode: "development"
+}];
