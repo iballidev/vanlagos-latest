ae949f600601bebb632f626d781e1139f43f3143
iballidev <iballi.dev@gmail.com>
2024-03-08 00:59:47 +0100

    update

---
+++ trash/user-review-service.php
@@ -0,0 +1,168 @@
+<?php include __DIR__ . "./includes/header.php"; ?>
+
+<div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+
+
+    <!-- user account navigation -->
+    <?php include __DIR__ . "./includes/user-account-navigation.php";?>
+    <!--  -->
+    <div class="my-4"></div>
+
+    <div class="mdc-card">
+        <h1
+            class="ff-headline font-semibold capitalize text-xl bg-dark text-white py-2 px-4 rounded-t-md sm:rounded-t-lg">
+            My Reviews
+        </h1>
+        <div class="p-2 sm:p-4">
+            <div class="relative overflow-x-auto mdc-card">
+                <!--  -->
+                <div class="mdc-tab-bar" role="tablist">
+                    <div class="mdc-tab-scroller">
+                        <div class="mdc-tab-scroller__scroll-area">
+                            <div class="mdc-tab-scroller__scroll-content">
+
+                                <button class="mdc-tab mdc-tab--active" role="tab" aria-selected="true" tabindex="0">
+                                    <span class="mdc-tab__content">
+                                        <span class="mdc-tab__icon material-icons material-icons text-md"
+                                            aria-hidden="true">
+                                            airport_shuttle
+                                        </span>
+                                        <span class="mdc-tab__text-label hidden sm:block text-sm capitalize">Van Hire
+                                            Service</span>
+                                    </span>
+                                    <span class="mdc-tab-indicator mdc-tab-indicator--active">
+                                        <span
+                                            class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
+                                    </span>
+                                    <span class="mdc-tab__ripple"></span>
+                                </button>
+
+                                <button class="mdc-tab" role="tab" aria-selected="true" tabindex="0">
+                                    <span class="mdc-tab__content">
+                                        <span class="mdc-tab__icon material-icons text-md">
+                                            directions_bus
+                                        </span>
+                                        <span class="mdc-tab__text-label hidden sm:block text-sm capitalize">Bus Hire
+                                            Service</span>
+                                    </span>
+                                    <span class="mdc-tab-indicator mdc-tab-indicator--active">
+                                        <span
+                                            class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
+                                    </span>
+                                    <span class="mdc-tab__ripple"></span>
+                                </button>
+
+                                <button class="mdc-tab" role="tab" aria-selected="true" tabindex="0">
+                                    <span class="mdc-tab__content">
+                                        <span class="mdc-tab__icon material-icons text-md">
+                                            directions_car
+                                        </span>
+                                        <span class="mdc-tab__text-label hidden sm:block text-sm capitalize">Car Hire
+                                            Service</span>
+                                    </span>
+                                    <span class="mdc-tab-indicator mdc-tab-indicator--active">
+                                        <span
+                                            class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
+                                    </span>
+                                    <span class="mdc-tab__ripple"></span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="p-4">
+                    <!-- Tab Content -->
+                    <div id="vh-tab1" class="vh-tab-content">
+                        <!-- daily van hire form -->
+                        <p>Rate Van Hire</p>
+                    </div>
+                    <div id="vh-tab2" class="hidden vh-tab-content">
+                        <!-- hourly van hire form -->
+                        <p>Rate Bus Hire</p>
+                    </div>
+                </div>
+
+
+
+
+
+
+
+
+                <!-- <div class="mdc-tab-bar" role="tablist">
+                    <div class="mdc-tab-scroller">
+                        <div class="mdc-tab-scroller__scroll-area">
+                            <div class="mdc-tab-scroller__scroll-content">
+
+                                <a href="./user-profile.php" class="mdc-tab mdc-tab--active" role="tab"
+                                    aria-selected="true" tabindex="0">
+                                    <span class="mdc-tab__content">
+                                        <span class="mdc-tab__icon material-icons material-icons text-md"
+                                            aria-hidden="true">
+                                            airport_shuttle
+                                        </span>
+                                        <span class="mdc-tab__text-label hidden sm:block text-sm capitalize">Van Hire
+                                            Service</span>
+                                    </span>
+                                    <span class="mdc-tab-indicator mdc-tab-indicator--active">
+                                        <span
+                                            class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
+                                    </span>
+                                    <span class="mdc-tab__ripple"></span>
+                                </a>
+
+                                <a href="./user-bookings.php" class="mdc-tab" role="tab" aria-selected="true"
+                                    tabindex="0">
+                                    <span class="mdc-tab__content">
+                                        <span class="mdc-tab__icon material-icons text-md">
+                                            directions_bus
+                                        </span>
+                                        <span class="mdc-tab__text-label hidden sm:block text-sm capitalize">Bus Hire
+                                            Service</span>
+                                    </span>
+                                    <span class="mdc-tab-indicator mdc-tab-indicator--active">
+                                        <span
+                                            class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
+                                    </span>
+                                    <span class="mdc-tab__ripple"></span>
+                                </a>
+
+                                <a href="./user-reviews.php" class="mdc-tab" role="tab" aria-selected="true"
+                                    tabindex="0">
+                                    <span class="mdc-tab__content">
+                                        <span class="mdc-tab__icon material-icons text-md">
+                                            directions_car
+                                        </span>
+                                        <span class="mdc-tab__text-label hidden sm:block text-sm capitalize">Car Hire
+                                            Service</span>
+                                    </span>
+                                    <span class="mdc-tab-indicator mdc-tab-indicator--active">
+                                        <span
+                                            class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
+                                    </span>
+                                    <span class="mdc-tab__ripple"></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div> -->
+                <!--  -->
+
+                <!-- <div class="my-2"></div> -->
+
+                <!-- user van hire review -->
+                <?php 
+                // include(__DIR__ . "./includes/user-van-hire-review.php"); 
+                ?>
+
+            </div>
+        </div>
+    </div>
+
+
+    <div class="my-4"></div>
+
+</div>
+
+<?php include __DIR__ . "./includes/footer.php"; ?>
