8b466abe18fe5fb5b175b1ae6314f99220d14e3b
iballidev <iballi.dev@gmail.com>
2024-03-08 01:56:09 +0100

    implement driver login and register pages

--- src/driver/driver-bookings.php
+++ src/driver/driver-bookings.php
@@ -1,3 +1,4 @@
+
 <?php include __DIR__ . "./includes/header.php"; ?>
 
 <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
--- src/driver/includes/driver-cancelled-bookings.php
+++ src/driver/includes/driver-cancelled-bookings.php
@@ -1,6 +1,6 @@
 <ul class="flex flex-col gap-4">
     <li>
-        <a href="#">
+        <a href="driver-booking-details.php">
             <div class="mdc-card p-4">
                 <div class="mdc-card__primary-action" tabindex="0">
                     <table class="text-sm">
@@ -8,7 +8,50 @@
                             <tr>
                                 <th rowspan="4">
                                     <div class="max-w-24">
-                                        <img src="./images/bus-icons-head.svg" alt="">
+                                        <img src="../images/bus-icons-head.svg" alt="">
+                                    </div>
+                                </th>
+                                <td class="text-primary font-bold text-2xl">Bus Hire</td>
+                                <td></td>
+                                <td class="text-end text-gray-300">1s ago</td>
+                            </tr>
+                            <tr>
+                                <td>Number of hours:</td>
+                                <td class="font-semibold text-end">4</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Total price:</td>
+                                <td class="font-bold text-2xl text-end">₦ 15,000</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>
+
+                                    <span
+                                        class="inline-flex items-center rounded-md bg-red-200 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">Cancelled</span>
+                                </td>
+                                <td class="text-danger text-end">not yet time</td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div class="mdc-card__ripple">
+                    </div>
+                </div>
+            </div>
+        </a>
+    </li>
+    <li>
+        <a href="driver-booking-details.php">
+            <div class="mdc-card p-4">
+                <div class="mdc-card__primary-action" tabindex="0">
+                    <table class="text-sm">
+                        <tbody>
+                            <tr>
+                                <th rowspan="4">
+                                    <div class="max-w-24">
+                                        <img src="../images/bus-icons-head.svg" alt="">
                                     </div>
                                 </th>
                                 <td class="text-primary font-bold text-2xl">Bus Hire</td>
@@ -28,7 +71,7 @@
                             <tr>
                                 <td>
                                     <span
-                                        class="inline-flex items-center rounded-md bg-blue-200 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">Started</span>
+                                        class="inline-flex items-center rounded-md bg-red-200 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">Cancelled</span>
                                 </td>
                                 <td class="text-danger text-end">not yet time</td>
                                 <td></td>
@@ -42,7 +85,7 @@
         </a>
     </li>
     <li>
-        <a href="#">
+        <a href="driver-booking-details.php">
             <div class="mdc-card p-4">
                 <div class="mdc-card__primary-action" tabindex="0">
                     <table class="text-sm">
@@ -50,49 +93,7 @@
                             <tr>
                                 <th rowspan="4">
                                     <div class="max-w-24">
-                                        <img src="./images/bus-icons-head.svg" alt="">
-                                    </div>
-                                </th>
-                                <td class="text-primary font-bold text-2xl">Bus Hire</td>
-                                <td></td>
-                                <td class="text-end text-gray-300">1s ago</td>
-                            </tr>
-                            <tr>
-                                <td>Number of hours:</td>
-                                <td class="font-semibold text-end">4</td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td>Total price:</td>
-                                <td class="font-bold text-2xl text-end">₦ 15,000</td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <span
-                                        class="inline-flex items-center rounded-md bg-yellow-200 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">Pending</span>
-                                </td>
-                                <td class="text-danger text-end">not yet time</td>
-                                <td></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <div class="mdc-card__ripple">
-                    </div>
-                </div>
-            </div>
-        </a>
-    </li>
-    <li>
-        <a href="#">
-            <div class="mdc-card p-4">
-                <div class="mdc-card__primary-action" tabindex="0">
-                    <table class="text-sm">
-                        <tbody>
-                            <tr>
-                                <th rowspan="4">
-                                    <div class="max-w-24">
-                                        <img src="./images/bus-icons-head.svg" alt="">
+                                        <img src="../images/bus-icons-head.svg" alt="">
                                     </div>
                                 </th>
                                 <td class="text-primary font-bold text-2xl">Van Hire</td>
@@ -112,7 +113,7 @@
                             <tr>
                                 <td>
                                     <span
-                                        class="inline-flex items-center rounded-md bg-blue-900 px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-white/10">Passed</span>
+                                        class="inline-flex items-center rounded-md bg-red-200 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">Cancelled</span>
                                 </td>
                                 <td class="text-danger text-end">not yet time</td>
                                 <td></td>
