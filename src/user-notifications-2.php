1f7f3ab0c0bb9baba3d40dfc47c33576903948a3
iballidev <iballi.dev@gmail.com>
2024-03-12 17:00:48 +0100

    implement design for notification-2

--- src/user-notifications-2.php
+++ src/user-notifications-2.php
@@ -13,16 +13,22 @@
 
         <ul class="notification-list-2 | flex flex-col gap-4 w-full p-4 text-sm flex-nowrap">
             <li>
-                <a href="https://yahoo.com" class="block p-4 mb-2 text-sm text-[#424662] visited:text-[#ff8c00]" role="alert">
-                    <strong>Reminder:&nbsp;</strong>Trip completed
+                <a href="https://yahoo.com" class="block p-2 sm:p-4 mb-2 text-sm text-[#424662] visited:text-[#ff8c00]" role="alert">
+                    <span><strong>Reminder:&nbsp;</strong>Trip completed</span>
+                    <span>1s</span>
                 </a>
-                <span class="text-light-500">1s</span>
             </li>
             <li>
-                <a href="https://aol.com" class="block p-4 mb-2 text-sm text-[#424662] visited:text-[#ff8c00]" role="alert">
-                    <strong>Reminder:&nbsp;</strong>Your have started the trip: 4 hours left 
+                <a href="https://aol.com" class="block p-2 sm:p-4 mb-2 text-sm text-[#424662] visited:text-[#ff8c00]" role="alert">
+                    <span><strong>Reminder:&nbsp;</strong>Your have started the trip: 4 hours left</span>
+                <span>3h</span>
                 </a>
-                <span class="text-light-500">3h</span>
+            </li>
+            <li>
+                <a href="https://ibrahimalli.com" class="block p-2 sm:p-4 mb-2 text-sm text-[#424662] visited:text-[#ff8c00]" role="alert">
+                    <span><strong>Reminder:&nbsp;</strong>Your have started the trip: 4 hours left</span>
+                    <span>4h</span>
+                </a>
             </li>
         </ul>
 
